76c97adae971c4d05d75539d9b522b35e303a6ef
Zerospace <zerospacedubs@gmail.com>
2020-02-12 11:43:52 -0500

    Updates

---
+++ precache-manifest.8d26b572203e956cd10db420da7c542c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757d368f042bedbba3cef3f544445107",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "09ac3730d901476c7b5b",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ac3730d901476c7b5b",
+    "url": "/dig4639-mobile-dev/static/js/main.080ddf28.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
